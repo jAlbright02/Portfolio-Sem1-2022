ae7f1171983a9a98e2949b92bf44dcef16902d36
Patrick <g00409592@atu.ie>
2022-10-11 14:15:35 +0100

    Added subtraction method. The method subtracts.

--- src/main/java/ie/atu/week4/Calc.java
+++ src/main/java/ie/atu/week4/Calc.java
@@ -1,9 +1,24 @@
 package ie.atu.week4;
 
+import java.util.Scanner;
+
 public class Calc
 {
     public static void main(String[] args)
     {
+        substract();
+    }
 
+    public static void substract()
+    {
+        System.out.print("Please enter your first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNum = inputs.nextInt();
+
+        System.out.print("Please enter your second number: ");
+        int secondNum = inputs.nextInt();
+
+        int subtractionResult = firstNum - secondNum;
+        System.out.println("The total is " + subtractionResult);
     }
 }
