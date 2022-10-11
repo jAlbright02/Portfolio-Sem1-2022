6225be874d6db4a89aa064a02bb740666e2abf62
Mark V <G00403931@atu.ie>
2022-10-11 14:28:27 +0100

    I created a new method called "add()"

--- src/main/java/ie/atu/week4/Calc.java
+++ src/main/java/ie/atu/week4/Calc.java
@@ -1,9 +1,26 @@
 package ie.atu.week4;
+import java.util.Scanner;
 
 public class Calc
 {
     public static void main(String[] args)
     {
-
+        add();
     }
+    public static void add()
+    {
+        //ASK USER TO INPUT NUMBER
+        System.out.println("Enter first number: ");
+        Scanner input = new Scanner(System.in);     //create scanner
+        int num_first = input.nextInt();
+
+        //ASK USER FOR SECOND NUMBER
+        System.out.println("Enter second number: ");
+        int num_second = input.nextInt();
+
+        //PRINTS CALCULATION
+        int sum = num_first + num_second;
+        System.out.println("The Sum is: " + sum);
+    }
+
 }
