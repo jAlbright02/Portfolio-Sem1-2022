309b2f08d2c28b259f4a954781d3ac5c5c650fd0
James Albright <G00379271@atu.ie>
2022-10-17 20:17:09 +0100

    prompt user for length

--- src/main/java/ie/atu/week5labtest/Shape.java
+++ src/main/java/ie/atu/week5labtest/Shape.java
@@ -1,4 +1,13 @@
 package ie.atu.week5labtest;
 
+import java.util.Scanner;
+
 public class Shape {
+    public static void main(String[] args) {
+        System.out.println("Please enter the width of the rectangle");
+        Scanner input = new Scanner(System.in);
+        double length = input.nextDouble();
+
+        System.out.println("You entered : " + length);
+    }
 }
