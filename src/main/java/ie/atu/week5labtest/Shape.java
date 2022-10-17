87faf80076f73342fc94e79c1509e7227465fc87
James Albright <G00379271@atu.ie>
2022-10-17 20:46:26 +0100

    Added new file and new methods

--- src/main/java/ie/atu/week5labtest/Shape.java
+++ src/main/java/ie/atu/week5labtest/Shape.java
@@ -4,14 +4,32 @@ import java.util.Scanner;
 
 public class Shape {
     public static void main(String[] args) {
+        //created first instance of rectangle
         System.out.println("Please enter the length of the rectangle");
         Scanner input = new Scanner(System.in);
+        Rectangle shapeRect = new Rectangle();
         double length = input.nextDouble();
-        System.out.println("You entered : " + length);
+        shapeRect.setLength(length);
+        System.out.println("You entered : " + shapeRect.getLength());
 
         System.out.println("Please enter the width of the rectangle");
         double width = input.nextDouble();
-        System.out.println("You entered : " + width);
+        shapeRect.setWidth(width);
+        System.out.println("You entered : " + shapeRect.getWidth());
+
+        //created second instance of rectangle
+        Rectangle shapeRect2 = new Rectangle();
+        System.out.println("Please enter the second length of the rectangle");
+        double length2 = input.nextDouble();
+        shapeRect2.setLength(length2);
+        System.out.println("You entered : " + shapeRect2.getLength());
+
+        System.out.println("Please enter the second width of the rectangle");
+        double width2 = input.nextDouble();
+        shapeRect2.setWidth(width2);
+        System.out.println("You entered : " + shapeRect2.getWidth());
+
+
 
     }
 }
