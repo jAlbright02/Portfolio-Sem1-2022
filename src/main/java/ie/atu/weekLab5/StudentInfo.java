015730da6694be4abe17e855a2199464d6e931a9
G00379271 <G00379271@atu.ie>
2022-10-18 13:46:08 +0100

    prompts user for information and prints out

--- src/main/java/ie/atu/weekLab5/StudentInfo.java
+++ src/main/java/ie/atu/weekLab5/StudentInfo.java
@@ -6,8 +6,21 @@ public class StudentInfo {
     public static void main(String[] args) {
 
         //first instance of student
-        System.out.println("");
+        System.out.println("Please enter first student's name:");
         Scanner studentInput = new Scanner(System.in);
+        Student infoStudent = new Student();
+        String studentName = studentInput.nextLine();
+        infoStudent.setNameStudent(studentName);
+
+        System.out.println("Please enter first student's email:");
+        String studentEmail = studentInput.nextLine();
+        infoStudent.setEmailStudent(studentEmail);
+
+        System.out.println("Please enter first student's course:");
+        String studentCourse = studentInput.nextLine();
+        infoStudent.setCourseStudent(studentCourse);
+
+        System.out.printf("Name: %s\nEmail: %s\nCourse: %s\n", studentName, studentEmail, studentCourse);
 
 
     }
