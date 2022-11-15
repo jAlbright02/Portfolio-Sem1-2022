9908ace7c9f47047e21adf0f977e03a8062e6808
James Albright <G00379271@atu.ie>
2022-11-15 13:36:08 +0000

    Added more checkers for the created file, using the methods from the file class

--- src/main/java/ie/atu/CreateFile.java
+++ src/main/java/ie/atu/CreateFile.java
@@ -7,6 +7,10 @@ public class CreateFile {
 
         File myFile = new File("MyFile.txt");
         System.out.println("My file is located at: " + myFile.getAbsolutePath());
+        System.out.println("My file length: " + myFile.length());
+        System.out.println("My file exists: " + myFile.exists());
+        System.out.println("My file was last edited: " + myFile.lastModified());
+
 
     }
 }
