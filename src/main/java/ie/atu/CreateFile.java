89a424790f8206841d9eb3e8306f06c5348d2e2e
James Albright <G00379271@atu.ie>
2022-11-15 13:40:14 +0000

    Added some comments to clarify that i created an instance

--- src/main/java/ie/atu/CreateFile.java
+++ src/main/java/ie/atu/CreateFile.java
@@ -5,6 +5,8 @@ import java.io.*;
 public class CreateFile {
     public static void main(String[] args) {
 
+
+        //created an instance myFile
         File myFile = new File("MyFile.txt");
         System.out.println("My file is located at: " + myFile.getAbsolutePath());
         System.out.println("My file length: " + myFile.length());
