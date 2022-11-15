399a65c92e4eae26f6b014267da186d1ceaca331
James Albright <G00379271@atu.ie>
2022-11-15 14:01:07 +0000

    Using the FileWriter class to write and append text to the txt file.
    As well as this, I added a try-catch to prevent any IO errors.

--- src/main/java/ie/atu/CreateFile.java
+++ src/main/java/ie/atu/CreateFile.java
@@ -2,8 +2,10 @@ package ie.atu;
 
 import java.io.*;
 
-public class CreateFile {
-    public static void main(String[] args) {
+public class CreateFile
+{
+    public static void main(String[] args)
+    {
 
 
         //created an instance myFile
@@ -13,6 +15,16 @@ public class CreateFile {
         System.out.println("My file exists: " + myFile.exists());
         System.out.println("My file was last edited: " + myFile.lastModified());
 
+        try
+        {
+            FileWriter outputFile = new FileWriter(myFile, true);
+            outputFile.write("This is a new file. Hello and goodbye.\n");
+            outputFile.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
 
     }
 }
