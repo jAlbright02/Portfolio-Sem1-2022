f889774a3c5281266556d43d30c987b2ab370163
James Albright <G00379271@atu.ie>
2022-11-16 16:18:33 +0000

    Try catch is now placed in a while loop. This runs until the user enters a valid number.

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -5,21 +5,25 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
+        //run in a while loop until user gives suitable entry
+        while (true) {
 
-        System.out.println("Enter a number between 0 - 9: ");
+            System.out.println("Enter a number between 0 - 9: ");
 
-        //try to run the code as expected
-        try {
-            Scanner userScan = new Scanner(System.in);
-            int userNum = userScan.nextInt();
+            //try to run the code as expected
+            try {
+                Scanner userScan = new Scanner(System.in);
+                int userNum = userScan.nextInt();
 
-            if (userNum <= 9 && userNum > 0) {
-                System.out.println("You entered " + userNum);
-            } else {
-                System.out.println("Not a valid number.");
+                if (userNum <= 9 && userNum > 0) {
+                    System.out.println("You entered " + userNum);
+                    break;
+                } else {
+                    System.out.println("Not a valid number.");
+                }
+            } catch (InputMismatchException e) { //catch the error as user gave unexpected input
+                System.out.println("Try enter a valid number.");
             }
-        } catch(InputMismatchException e) { //catch the error as user gave unexpected input
-            System.out.println("Invalid entry.");
         }
     }
 }
