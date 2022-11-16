a487b9385b2f2f2cdc96e36a31ab6d636ed28d87
James Albright <G00379271@atu.ie>
2022-11-16 16:14:41 +0000

    Added a try catch function to stop mismatched inputs and warn user

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -1,18 +1,25 @@
 package ie.atu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args) {
+
         System.out.println("Enter a number between 0 - 9: ");
-        Scanner userScan = new Scanner(System.in);
-        int userNum = userScan.nextInt();
 
-        if(userNum <= 9 && userNum > 0) {
-            System.out.println("You entered " + userNum);
-        } else{
-            System.out.println("Not a valid number.");
+        //try to run the code as expected
+        try {
+            Scanner userScan = new Scanner(System.in);
+            int userNum = userScan.nextInt();
+
+            if (userNum <= 9 && userNum > 0) {
+                System.out.println("You entered " + userNum);
+            } else {
+                System.out.println("Not a valid number.");
+            }
+        } catch(InputMismatchException e) { //catch the error as user gave unexpected input
+            System.out.println("Invalid entry.");
         }
-
     }
 }
