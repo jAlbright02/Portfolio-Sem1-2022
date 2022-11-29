f07a9a5a24745b1882b697198700855bf120feb2
G00379271 <G00379271@atu.ie>
2022-11-29 13:19:16 +0000

    created Person class

---
+++ src/main/java/ie/atu/Person.java
@@ -0,0 +1,41 @@
+package ie.atu;
+
+public class Person {
+
+    private String namePerson;
+    private String addressPerson;
+    private String phonePerson;
+
+    public Person(){
+    }
+
+    public Person(String namePerson, String addressPerson, String phonePerson){
+        this.namePerson = namePerson;
+        this.addressPerson = addressPerson;
+        this.phonePerson = phonePerson;
+    }
+
+    public void setNamePerson(String namePerson) {
+        this.namePerson = namePerson;
+    }
+
+    public void setAddressPerson(String addressPerson) {
+        this.addressPerson = addressPerson;
+    }
+
+    public void setPhonePerson(String phonePerson) {
+        this.phonePerson = phonePerson;
+    }
+
+    public String getNamePerson() {
+        return namePerson;
+    }
+
+    public String getAddressPerson() {
+        return addressPerson;
+    }
+
+    public String getPhonePerson() {
+        return phonePerson;
+    }
+}
