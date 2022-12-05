598560ba6a6600e8c770cf152807605c9996384f
James Albright <G00379271@atu.ie>
2022-12-05 19:49:03 +0000

    created class called Customer and added basic getters, setters and constructors. Also inherits from the Person class.

---
+++ src/main/java/ie/atu/Customer.java
@@ -0,0 +1,35 @@
+package ie.atu;
+
+public class Customer extends Person{
+
+    private String customerNum;
+
+    private Boolean mailingList;
+
+
+    public Customer(String customerNum, Boolean mailingList) {
+
+    }
+
+    public Customer(String name, String address, String number, String customerNum, Boolean mailingList) {
+        super(name, address, number);
+        this.customerNum = customerNum;
+        this.mailingList = mailingList;
+    }
+
+    public String getCustomerNum() {
+        return customerNum;
+    }
+
+    public void setCustomerNum(String customerNum) {
+        this.customerNum = customerNum;
+    }
+
+    public Boolean getMailingList() {
+        return mailingList;
+    }
+
+    public void setMailingList(Boolean mailingList) {
+        this.mailingList = mailingList;
+    }
+}
